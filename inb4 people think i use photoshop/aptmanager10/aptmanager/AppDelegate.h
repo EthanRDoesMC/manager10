7aaaabe509ed14e645939dfffc3b698cfd664ee5
EthanRDoesMC <EthanRDoesMC@gmail.com>
2017-03-11 18:05:26 -0500

    Added xcode stuff

---
+++ inb4 people think i use photoshop/aptmanager10/aptmanager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aptmanager
+//
+//  Created by EthanR on 3/4/17.
+//  Copyright © 2017 EthanRDoesMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
